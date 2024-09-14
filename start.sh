9e6643626c862edc76f42627dac504762e9fd282
ComicSans <tobias@nerdwana.net>
2024-09-14 18:20:18 +0200

    docker update

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 
 cd docker-compose
 docker compose -f $1 stop
-docker compose -f $1 -p $1 up -d --remove-orphans
-#docker compose -f $1 -p $1 up -d --pull=always --remove-orphans
+#docker compose -f $1 -p $1 up -d --remove-orphans
+docker compose -f $1 -p $1 up -d --pull=always --remove-orphans
 cd ..
